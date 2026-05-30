5df9ccdf37cf842b54da8538c4742a37b50a52f2
Daniel Müller <deso@posteo.net>
2026-05-30 12:03:32 -0700

    Return ExitCode from main()
    Returning a Result from main() causes the standard library to emit the
    contained error's Debug impl, but it prefixes that with "Error: ", which
    often causes weird "Error: Error: " strings to be emitted to the user.
    Don't rely on that infrastructure but just print errors yourself and
    indicate success/failure via an ExitCode.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
-// Copyright (C) 2025 Daniel Mueller <deso@posteo.net>
+// Copyright (C) 2025-2026 Daniel Mueller <deso@posteo.net>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 //! Now Playing Daemon (`npd`) is a program for sending `DBus`
 //! notifications when the song currently played by MPD changes.
 
-use anyhow::Result;
+use std::process::ExitCode;
 
 use npd::run;
 
 
-fn main() -> Result<()> {
+fn main() -> ExitCode {
   run()
+    .map(|_| ExitCode::SUCCESS)
+    .map_err(|e| eprintln!("{e:?}"))
+    .unwrap_or(ExitCode::FAILURE)
 }
